aa5c2040001a1cbfd2da4ed5774845effc83bc06
BRENO_MORAIS <brenofranciscomorais@gmail.com>
2021-04-19 19:21:57 -0400

    Init

---
+++ CompactadorMulti/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace CompactadorMulti
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCompactar = new System.Windows.Forms.TextBox();
+            this.txtEnviar = new System.Windows.Forms.TextBox();
+            this.btnCompactar = new System.Windows.Forms.Button();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.dgvResultado = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCompactar
+            // 
+            this.txtCompactar.Location = new System.Drawing.Point(12, 246);
+            this.txtCompactar.Name = "txtCompactar";
+            this.txtCompactar.Size = new System.Drawing.Size(368, 23);
+            this.txtCompactar.TabIndex = 0;
+            // 
+            // txtEnviar
+            // 
+            this.txtEnviar.Location = new System.Drawing.Point(12, 300);
+            this.txtEnviar.Name = "txtEnviar";
+            this.txtEnviar.Size = new System.Drawing.Size(368, 23);
+            this.txtEnviar.TabIndex = 1;
+            // 
+            // btnCompactar
+            // 
+            this.btnCompactar.Location = new System.Drawing.Point(404, 227);
+            this.btnCompactar.Name = "btnCompactar";
+            this.btnCompactar.Size = new System.Drawing.Size(116, 42);
+            this.btnCompactar.TabIndex = 3;
+            this.btnCompactar.Text = "Compactar";
+            this.btnCompactar.UseVisualStyleBackColor = true;
+            this.btnCompactar.Click += new System.EventHandler(this.btnCompactar_Click);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(404, 283);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(116, 42);
+            this.btnEnviar.TabIndex = 4;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // dgvResultado
+            // 
+            this.dgvResultado.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvResultado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultado.Location = new System.Drawing.Point(12, 12);
+            this.dgvResultado.Name = "dgvResultado";
+            this.dgvResultado.RowHeadersWidth = 100;
+            this.dgvResultado.RowTemplate.Height = 25;
+            this.dgvResultado.Size = new System.Drawing.Size(506, 209);
+            this.dgvResultado.TabIndex = 5;
+            this.dgvResultado.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 413);
+            this.Controls.Add(this.dgvResultado);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.btnCompactar);
+            this.Controls.Add(this.txtEnviar);
+            this.Controls.Add(this.txtCompactar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCompactar;
+        private System.Windows.Forms.TextBox txtEnviar;
+        private System.Windows.Forms.Button btnCompactar;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.DataGridView dgvResultado;
+    }
+}
+
---
+++ CompactadorMulti/Form1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+namespace CompactadorMulti
+{
+    public partial class Form1 : Form
+    {
+
+        public static DirectoryInfo directory = new DirectoryInfo(@"D:\_DESENVOLVIMENTO\CODIGO FONTE");
+        public static string[] GetDirectory = Directory.GetDirectories(@"D:\_DESENVOLVIMENTO\CODIGO FONTE\");
+
+        Collection<Arquivos> lista = new Collection<Arquivos>();
+        FileInfo[] arquivos = directory.GetFiles(@"*.*");
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            Arquivos obj = new Arquivos();
+            //dataGridView1.ColumnAdded("teste");
+            foreach (var item in GetDirectory)
+            {
+
+                lista.Add(new Arquivos { Nome = item });
+
+            }
+
+
+
+            //var a = arquivos;
+            //dataGridView1.Width = 20000;
+
+            dgvResultado.DataSource = lista;
+
+        }
+
+        public class Arquivos
+        {
+            public string Nome { get; set; }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var name = dgvResultado.Rows[e.RowIndex].Cells[0].Value;
+            txtCompactar.Text = name.ToString();
+            var a = lista.First(a => a.Nome == name);
+            lista.Remove(a);
+
+
+
+
+        }
+
+        private async void btnCompactar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string diretorio = @"D:\_DESENVOLVIMENTO\CODIGO FONTE\Nova pasta";
+                string arquivo = txtCompactar.Text /*=/* txtCompactar.Text + @"D:\_DESENVOLVIMENTO\CODIGO FONTE\blogDev.rar"*/ ;
+                if (File.Exists(arquivo))
+                {
+
+                  
+
+
+                    File.Delete(arquivo);
+                }
+                else
+                {
+              
+                    ZipFile.CreateFromDirectory(diretorio, arquivo+".zip");
+                  
+                    MessageBox.Show("Concluido", "");
+                }
+
+            }
+            catch (Exception a)
+            {
+                Console.WriteLine(a);
+            }
+        
+
+
+        }
+    }
+}
